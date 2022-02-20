2850727a49da03c6ad7c2ea26d3525e706e955d3
Tiago Alves <tiago_lima411@hotmail.com>
2022-02-20 09:58:17 -0300

    add health spec

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -3,6 +3,7 @@ import { HealthController } from './health.controller';
 
 describe('HealthController', () => {
   let controller: HealthController;
+  let result: any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +11,18 @@ describe('HealthController', () => {
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
+    result = controller.check();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('.check', () => {
+    it('resturns expected attributes', () => {
+      expect(result.status).toEqual('ok');
+      expect(result.ts).toBeDefined();
+      expect(result.version).toEqual('0.0.2');
+    });
   });
 });
