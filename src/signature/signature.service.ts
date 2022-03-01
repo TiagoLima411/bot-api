1de240e550718310a3df8a7df943e0ff50891656
Tiago Alves <tiago_lima411@hotmail.com>
2022-03-01 11:25:43 -0300

    fix test

--- src/signature/signature.service.ts
+++ src/signature/signature.service.ts
@@ -8,8 +8,10 @@ export class SignatureService {
   async create(req: any) {
     const secret = configService.get<string>('API_SECRET');
     const timestamp = Date.now();
+    const query = req.query || '';
+    const body = req.body || '';
 
-    const prehash = `${timestamp}${req.method}${req.url}${req.query}${req.body}`;
+    const prehash = `${timestamp}${req.method}${req.url}${query}${body}`;
     const result = await crypto.HmacSHA256(prehash, secret).toString();
 
     return result;
