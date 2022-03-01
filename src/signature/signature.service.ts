3fab0ef55bdf46f318114e342b90187eb47629d4
Tiago Alves <tiago_lima411@hotmail.com>
2022-03-01 11:13:39 -0300

    add async

--- src/signature/signature.service.ts
+++ src/signature/signature.service.ts
@@ -5,12 +5,12 @@ import * as crypto from 'crypto-js';
 const configService = new ConfigService();
 @Injectable()
 export class SignatureService {
-  create(req: any) {
+  async create(req: any) {
     const secret = configService.get<string>('API_SECRET');
     const timestamp = Date.now();
 
     const prehash = `${timestamp}${req.method}${req.url}${req.query}${req.body}`;
 
-    return crypto.HmacSHA256(prehash, secret).toString();
+    return await crypto.HmacSHA256(prehash, secret).toString();
   }
 }
