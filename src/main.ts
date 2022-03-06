55cadf2521237a3593d52e1666f48ed5ce1db8ea
Tiago Alves <tiago_lima411@hotmail.com>
2022-03-05 23:14:28 -0300

    remove fastify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
 const configService = new ConfigService();
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
-  await app.listen(
-    configService.get<number>('PORT') || 3000,
-    configService.get<string>('SERVER_ADDR' || null),
-  );
+  const app = await NestFactory.create(AppModule);
+  await app.listen(configService.get<number>('PORT') || 3000);
 
   console.log(`Aplication is running on: ${await app.getUrl()}`);
 }
