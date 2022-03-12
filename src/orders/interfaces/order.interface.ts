fc7febea5d32f698af80b086963bfd20526ff3e6
Tiago Alves <tiago_lima411@hotmail.com>
2022-03-12 18:03:42 -0300

    remove unused interfaces

--- src/orders/interfaces/order.interface.ts
+++ src/orders/interfaces/order.interface.ts
@@ -36,29 +36,3 @@ export interface Order extends Document {
   readonly created_at: string;
   readonly trades_count: number;
 }
-
-export interface ICreatedOrderDto {
-  readonly sn: string;
-  readonly client_order_id: string;
-  readonly market_symbol: string;
-  readonly side: typeof SideEnum;
-  readonly type: typeof TypeEnum;
-  readonly state: typeof StateEnum;
-  readonly price: string;
-  readonly price_avg: string;
-  readonly quantity: string;
-  readonly quantity_executed: string;
-  readonly instant_amount: string;
-  readonly instant_amount_executed: string;
-  readonly created_at: string;
-  readonly trades_count: number;
-}
-
-export interface ICreateOrderDto {
-  readonly side: typeof SideEnum;
-  readonly type: typeof TypeEnum;
-  readonly market_symbol: string;
-  readonly client_order_id: string;
-  readonly remark: string;
-  readonly quantity: string;
-}
